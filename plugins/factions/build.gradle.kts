c926bd90fcd20966ed7c6a00e47d5fb6a72748f2
victorchimenton <30086298+victorchimenton@users.noreply.github.com>
2025-06-16 15:11:37 -0300

    relocate Jackson and MongoJack to prevent conflicts with server versions

--- plugins/factions/build.gradle.kts
+++ plugins/factions/build.gradle.kts
@@ -15,6 +15,15 @@ tasks {
     shadowJar {
         archiveClassifier.set("")
 
+        // --- THE FIX IS HERE ---
+        // Relocate Jackson to prevent conflicts with the server's built-in version.
+        // This is the direct solution to the NoSuchFieldError.
+        relocate("com.fasterxml.jackson", "dev.balafini.libs.jackson")
+
+        // It's also best practice to relocate MongoJack itself.
+        relocate("org.mongojack", "dev.balafini.libs.mongojack")
+
+        // Your existing relocations are also correct.
         relocate("org.incendo.cloud", "dev.balafini.libs.cloud")
         relocate("com.github.benmanes.caffeine", "dev.balafini.libs.caffeine")
         relocate("com.mongodb", "dev.balafini.libs.mongodb")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,9 +2,5 @@ rootProject.name = "factions-server"
 
 include(
     "plugins:factions",
-    "plugins:factions-core",
 )
 
-
-
-include("plugins:factions-data")
